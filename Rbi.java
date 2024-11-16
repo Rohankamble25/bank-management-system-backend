176af6775fc61c3b391304b6708445ff49409a76
Rohankamble25 <rohankamble2516@gmail.com>
2024-11-17 00:06:44 +0530

    Add files via upload

---
+++ Rbi.java
@@ -0,0 +1,13 @@
+package com.cjc.bms.servicei;
+
+public interface Rbi {
+	public void  Createaccount();
+	public void  DisplayAccountDetails();
+	public void DepositeMoney();
+	public void  WidrawlMoney();
+	public void CheckBalance();
+	
+	
+
+
+}
